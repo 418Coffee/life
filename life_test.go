9ee5f6bcf0d453cb50162247d2d715583a58410b
418Coffee <matthijskes@gmail.com>
2022-05-07 16:06:47 +0200

    refactor tests

--- life_test.go
+++ life_test.go
@@ -5,21 +5,89 @@ import (
 	"testing"
 )
 
-func TestExample(t *testing.T) {
-	l, err := LoadGame("./examples/glider.rle", true)
-	if err != nil {
-		t.Fatal(err)
+func TestExamples(t *testing.T) {
+	testCases := []struct {
+		filepath      string
+		width, height uint
+		wrap          bool
+		field         [][]bool
+	}{
+		{
+			filepath: "./examples/bi-gun.rle",
+			width:    50, height: 15,
+			wrap: true,
+			field: [][]bool{
+				{false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, true, true},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, true, true},
+				{false, false, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, false, false},
+				{true, true, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{true, true, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false},
+			},
+		},
+		{
+			filepath: "./examples/glider.rle",
+			width:    3, height: 3,
+			wrap: true,
+			field: [][]bool{
+				{false, true, false},
+				{false, false, true},
+				{true, true, true},
+			},
+		},
+		{
+			filepath: "./examples/inverter.rle",
+			width:    36, height: 20,
+			wrap: true,
+			field: [][]bool{
+				{false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, true, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, true},
+				{false, false, false, false, false, false, false, false, false, false, false, true, true, false, true, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, true, true},
+				{true, true, false, false, false, false, false, false, false, false, true, true, true, false, true, false, false, true, false, false, false, true, false, false, false, true, false, false, false, false, false, false, false, false, false, false},
+				{true, true, false, false, false, false, false, false, false, false, false, true, true, false, true, false, true, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+			},
+		},
 	}
-	if l.width != 3 || l.height != 3 {
-		t.Fatalf("unexpected width height: %d %d", l.width, l.height)
-	}
-	if !reflect.DeepEqual(l.current.s[0], []bool{false, true, false}) {
-		t.FailNow()
-	}
-	if !reflect.DeepEqual(l.current.s[1], []bool{false, false, true}) {
-		t.FailNow()
-	}
-	if !reflect.DeepEqual(l.current.s[2], []bool{true, true, true}) {
-		t.FailNow()
+
+	for _, test := range testCases {
+		t.Run(test.filepath, func(t *testing.T) {
+			l, err := LoadGame(test.filepath, test.wrap)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if l.width != test.width || l.height != test.height {
+				t.Errorf("got width height: %d %d, wanted width height: %d %d", l.width, l.height, test.width, test.height)
+			}
+			for i, line := range l.current.s {
+				if !reflect.DeepEqual(line, test.field[i]) {
+					t.Errorf("line %d does not line up", i)
+				}
+
+			}
+		})
 	}
 }
